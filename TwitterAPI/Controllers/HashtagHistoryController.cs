d0641e00f69502aa434eb7851014e7ceb8cfa9ab
Alejandro <alevelara@gmail.com>
2019-12-04 23:19:56 +0100

    Adding historical controller

---
+++ TwitterAPI/Controllers/HashtagHistoryController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TwitterAPI.Models;
+using TwitterAPI.Repositories.Interfaces;
+
+namespace TwitterAPI.Controllers
+{
+    [Route("api/HashtagHistory")]
+    [ApiController]
+    public class HashtagHistoryController : ControllerBase
+    {
+        private readonly IHistoricalHashtagRepository _repo;
+
+        public HashtagHistoryController(IHistoricalHashtagRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet]
+
+        public async Task<ActionResult> GetHistorical()
+        {
+            var historical =  await _repo.GetAsyncHistoricalHashtags().ConfigureAwait(false);
+
+            return Ok(historical);
+        }
+
+    }
+}
